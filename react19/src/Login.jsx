7855b9226f61dca84b508be09b41166461b60528
mikyllasd <ty202500691@wsmu.edu.ph>
2026-09-05 00:11:55 +0800

    useState Login

--- react19/src/Login.jsx
+++ react19/src/Login.jsx
@@ -1,28 +1,29 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 export const Login = () => {
-    const[email, setEmail] = useState("")
-    const[password, setPassword] = useState ("")
+    const [email, setEmail] = useState ("")
+    const [password, setPassword] = useState("")
 
     const handleClick = (e) => {
-        e.preventDefault () 
+        e.preventDefault()
         console.log("Email: ", email)
-        console.log("Password: ", password)
+        console.log("Password: ",password)
     }
+
     return (
         <div>
             <form onSubmit={handleClick}>
-            <label htmlFor="">Email: </label>
-            <input type="email"
-            value={email}
-            onChange={(e)=> setEmail (e.target.value)} />
-            <br></br>
-            <label htmlFor="">Password: </label>
-            <input type="password"
-            value={password}
-            onChange={(e)=> setPassword (e.target.value)} />
-            <br />
-            <button type = "submit">Login</button>
+                <label htmlFor="email">Email: </label>
+                <input type="email"
+                value={email}
+                onChange={(e)=> setEmail (e.target.value)} />
+                <br></br>
+                <label htmlFor="password">Password: </label>
+                <input type="password"
+                value={password}
+                onChange={(e)=> setPassword(e.target.value)} />
+                <br />
+                <button type="submit">Login</button>
             </form>
         </div>
     )
