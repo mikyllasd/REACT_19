2069e61429879aacf34c7afa32ff77d388310435
mikyllasd <ty202500691@wsmu.edu.ph>
2026-09-04 23:53:29 +0800

    useState

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -11,12 +11,14 @@ import {UserDetails} from './UserDetails'
 import {ProductList} from './ProductList'
 import {Alert} from './Alert'
 import {CustomButton} from './CustomButton'
+import { Login } from './Login'
 import './App.css'
 
 function App() {
   return (
     <div>
-      <CustomButton />
+      <Login />
+      {/* <CustomButton /> */}
       {/* <Alert>Your changes have been saved</Alert>
       <Alert type = "error">Your changes have been saved</Alert> */}
       {/* <ProductList /> */}
---
+++ react19/src/Login.jsx
@@ -0,0 +1,29 @@
+import {useState} from 'react'
+
+export const Login = () => {
+    const[email, setEmail] = useState("")
+    const[password, setPassword] = useState ("")
+
+    const handleClick = (e) => {
+        e.preventDefault () 
+        console.log("Email: ", email)
+        console.log("Password: ", password)
+    }
+    return (
+        <div>
+            <form onSubmit={handleClick}>
+            <label htmlFor="">Email: </label>
+            <input type="email"
+            value={email}
+            onChange={(e)=> setEmail (e.target.value)} />
+            <br></br>
+            <label htmlFor="">Password: </label>
+            <input type="password"
+            value={password}
+            onChange={(e)=> setPassword (e.target.value)} />
+            <br />
+            <button type = "submit">Login</button>
+            </form>
+        </div>
+    )
+}
